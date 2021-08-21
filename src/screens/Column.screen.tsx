c92b12cec546ae4f13369de078624d12d694ac1c
Alexander Ikonnikov <sashaikonnikov55@gmail.com>
2021-08-21 10:54:02 +0600

    feat: Added logout modal

--- src/screens/Column.screen.tsx
+++ src/screens/Column.screen.tsx
@@ -6,19 +6,42 @@ import { AppText } from "../ui/AppText";
 import { Setting } from "../ui/icons/Setting";
 import { ColumnScreenRouteProp } from '../types';
 import { StyledContainer } from "../ui/StyledContainer";
+import { AppModal } from "../ui/AppModal";
+import { useState } from "react";
+import { Button } from "../ui/Button";
+import { useAppDispatch } from "../store/hooks";
+import { actions } from "../store/ducks";
 
 export const Column: FC = () => {
     const route = useRoute<ColumnScreenRouteProp>();
+    const dispatch = useAppDispatch();
+    const [stateModal, setStateModal] = useState(false);
 
+    const openModal = () => {
+        setStateModal(true);
+    }
+
+    const closeModal = ()=> {
+        setStateModal(false);
+    }
+
+    const logOut = () => {
+        dispatch(actions.user.logOut());
+    }
     return (
         <React.Fragment>
             <Header withTab>
                 <StyledContainer containerStyled={`flex-grow: 1; align-items: center;`}>
                     <AppText bold>{route.params.name}</AppText>
                 </StyledContainer>
-                <Setting />
+                <Setting onPress={openModal} />
             </Header>
             <TabRoute/>
+            <AppModal visible={stateModal}>
+                <AppText>Do you want to log out?</AppText>
+                <Button title="YES" onPress={logOut} />
+                <Button title="NO" onPress={closeModal}/>
+            </AppModal>
         </React.Fragment>
     )
 };
